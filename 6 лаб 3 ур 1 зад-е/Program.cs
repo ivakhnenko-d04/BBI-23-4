7ee17df89327226f509acd5f04ef073fed971174
Daria <Daria@LAPTOP-JP3RUSAJ>
2024-05-14 14:01:06 +0300

    6лаб 3 ур 1 зад-е

---
+++ 6 лаб 3 ур 1 зад-е/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_лаб_3_ур_1_зад_е
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int N = 3;
+            const int marksNumber = 3;
+            Console.WriteLine("Hello, World!");
+            session sess = new session();
+            sess.groups = new group[N];
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("enter group name : " + (i + 1));
+                string groupName = Console.ReadLine();
+                Console.WriteLine("enter marks");
+                int[] marks = new int[marksNumber];
+                for (int j = 0; j < marksNumber; j++)
+
+                    marks[j] = Convert.ToInt32(Console.ReadLine());
+                sess.groups[i] = new group(marks, groupName);
+
+
+
+
+            }
+            bool flag = true;
+            while (flag)
+            {
+                flag = false;
+                for (int i = 0; i < N - 1; i++)
+                {
+                    if (sess.groups[i].getAverageMark() < sess.groups[i + 1].getAverageMark())
+                    {
+                        group buf = sess.groups[i];
+                        sess.groups[i] = sess.groups[i + 1];
+                        sess.groups[i + 1] = buf;
+                        flag = true;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("Average marks for groups");
+            for (int i = 0; i < N; i++)
+                Console.WriteLine(sess.groups[i].getGroupName() + " " + sess.groups[i].getAverageMark());
+        }
+
+
+        struct session
+        {
+            public group[] groups;
+        }
+        struct group
+        {
+            private int[] marks;
+            private string groupName;
+            private double averageMark;
+
+            public int[] getMarks()
+            {
+                return marks;
+            }
+            public string getGroupName()
+            {
+                return groupName;
+            }
+
+            public double getAverageMark()
+            {
+                return averageMark;
+            }
+
+            public group(int[] m, string n)
+            {
+                marks = m;
+                groupName = n;
+                averageMark = 0;
+                averageMark = calculateAverageMark();
+            }
+            double calculateAverageMark()
+            {
+                double a = 0;
+                for (int i = 0; i < marks.Length; i++)
+                {
+                    a += marks[i];
+                }
+                a = a / marks.Length;
+                return a;
+            }
+        }
+
+
+    }
+}
---
+++ 6 лаб 3 ур 1 зад-е/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("6 лаб 3 ур 1 зад-е")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("6 лаб 3 ур 1 зад-е")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("acdb5076-eb19-4801-b171-c9b6f2ea8275")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
